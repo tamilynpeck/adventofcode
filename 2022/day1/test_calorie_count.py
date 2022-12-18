75313ae109f58e506b9fb0f273bc91fcbb600931
Tamilyn <tamilyn.peck2@extendhealth.com>
2022-12-17 22:41:33 -0700

    2022 day 1

--- 2022/day1/test_calorie_count.py
+++ 2022/day1/test_calorie_count.py
@@ -1,6 +1,24 @@
-file = "test_input.txt"
+from calorie_counting import calorie_counting
 
-with open(file) as input_file:
-    data = [int(line.strip()) for line in input_file.readlines()]
 
-print(data)
+def test_elf_with_most_calories():
+    file = "test_input.txt"
+
+    result = calorie_counting(file)
+    assert result == 24000
+
+
+def test_real_input():
+    PUZZLE_INPUT_FILE = "input.txt"
+    result = calorie_counting(PUZZLE_INPUT_FILE)
+
+    print("result", result)
+    assert result == 71471
+
+
+def test_real_input_part_two():
+    PUZZLE_INPUT_FILE = "input.txt"
+    result = calorie_counting(PUZZLE_INPUT_FILE, top=3)
+
+    print("result", result)
+    assert result == 211189
