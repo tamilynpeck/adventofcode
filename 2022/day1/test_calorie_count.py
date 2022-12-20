cc492f01eeb93dc3d904c4445c3524fca42bddb3
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2022-12-20 14:29:07 -0700

    additional day1 input test

--- 2022/day1/test_calorie_count.py
+++ 2022/day1/test_calorie_count.py
@@ -9,16 +9,31 @@ def test_elf_with_most_calories():
 
 
 def test_real_input():
-    PUZZLE_INPUT_FILE = "input.txt"
-    result = calorie_counting(PUZZLE_INPUT_FILE)
+    puzzle_input_file = "input.txt"
+    result = calorie_counting(puzzle_input_file)
 
     print("result", result)
     assert result == 71471
 
 
 def test_real_input_part_two():
-    PUZZLE_INPUT_FILE = "input.txt"
-    result = calorie_counting(PUZZLE_INPUT_FILE, top=3)
+    puzzle_input_file = "input.txt"
+    result = calorie_counting(puzzle_input_file, top=3)
 
     print("result", result)
     assert result == 211189
+
+
+def test_real_input_b():
+    puzzle_input_file = "input_b.txt"
+    result = calorie_counting(puzzle_input_file)
+
+    print("result", result)
+    assert result == 70374
+
+def test_real_input_part_two_b():
+    puzzle_input_file = "input_b.txt"
+    result = calorie_counting(puzzle_input_file, top=3)
+
+    print("result", result)
+    assert result == 204610
