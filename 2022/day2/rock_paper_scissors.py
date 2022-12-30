e9b18924232dacb140299e9e7c39cb098c99e85f
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2022-12-30 15:52:24 -0700

    progress

--- 2022/day2/rock_paper_scissors.py
+++ 2022/day2/rock_paper_scissors.py
@@ -1,49 +1,77 @@
 from FileReader import FileReader
 
 
-# Rock defeats Scissors, Scissors defeats Paper, Paper defeats Rock
-class Score:
-    opponent_mapping = {"A": "Rock", "B": "Paper", "C": "Scissors"}
-    personal_mapping = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
-    personal_mapping = {"X": "Lose", "Y": "Draw", "Z": "Win"}
-    score_mapping = {
-        "Rock": 1,
-        "Paper": 2,
-        "Scissors": 3,
-        "Lost": 0,
-        "Draw": 3,
-        "Win": 6,
-    }
-    rules = {"Rock": "Scissors", "Scissors": "Paper", "Paper": "Rock"}
+class ScoreCalculator:
+    winner_of_action = {"Rock": "Scissors", "Scissors": "Paper", "Paper": "Rock"}
+    loser_of_action = {v: k for k, v in winner_of_action.items()}
 
-    @staticmethod
-    def score(opponent, personal):
+    def __init__(self):
+        self.action_mapping = {"A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors"}
+        self.result_mapping = {"X": "Lose", "Y": "Draw", "Z": "Win"}
+        self.score_mapping = {
+            "Rock": 1,
+            "Paper": 2,
+            "Scissors": 3,
+            "Lost": 0,
+            "Draw": 3,
+            "Win": 6,
+        }
+
+    def score(self, first_value, second_value):
         score = 0
-        opponent = Score.opponent_mapping.get(opponent)
-        personal = Score.personal_mapping.get(personal)
+        opponent_action = self.action_mapping.get(first_value)
+        action = self.action_mapping.get(second_value)
 
-        score += Score.score_mapping.get(personal)
+        score += self.score_mapping.get(action)
 
-        if opponent == personal:
-            score += Score.score_mapping.get("Draw")
-
-        if Score.rules.get(personal) == opponent:
-            score += Score.score_mapping.get("Win")
-
-        if Score.rules.get(opponent) == personal:
-            score += Score.score_mapping.get("Lost")
+        result = self.result_of_actions(opponent_action, action)
+        score += self.score_mapping.get(result)
 
         return score
 
+    def result_of_actions(self, opponent_action, action):
+        if opponent_action == action:
+            return "Draw"
 
-def rock_paper_scissors(input_file):
+        if self.winner_of_action.get(action) == opponent_action:
+            return "Win"
+
+        "Lost"
+
+
+    def score_part2(self, first_value, second_value):
+        score = 0
+        opponent_action = self.action_mapping.get(first_value)
+        expected_result = self.result_mapping.get(second_value)
+        print(expected_result)
+        
+        score += self.score_mapping.get(expected_result)
+
+        expected_action = ScoreCalculator.action_for_expected_result(opponent_action, expected_result)
+        score += self.score_mapping.get(expected_action)
+
+        return score
+
+    def action_for_expected_result(opponent_action, expected_result):
+        if expected_result == "Draw":
+            return opponent_action
+
+        if expected_result == "Win":
+            return ScoreCalculator.winner_of_action.get(expected_result)
+
+        if expected_result == "Lost":
+            return ScoreCalculator.loser_of_action.get(expected_result)
+
+
+def rock_paper_scissors(input_file, default_calc=True):
     data = FileReader.read_txt(input_file)
     guide = [[line[0], line[2]] for line in data]
-    print(guide)
+    
+    calculator = ScoreCalculator()
     score = 0
 
     for round in guide:
-        score += Score.score(round[0], round[1])
+        score += calculator.score(round[0], round[1]) if default_calc else calculator.score_part2(round[0], round[1])
 
     return score
 
@@ -51,4 +79,7 @@ def rock_paper_scissors(input_file):
 if __name__ == "__main__":
     PUZZLE_INPUT_FILE = "input.txt"
     result = rock_paper_scissors(PUZZLE_INPUT_FILE)
+    print(result)
+
+    result = rock_paper_scissors(PUZZLE_INPUT_FILE, default_calc=False)
     print(result)
--- 2022/day2/test_rock_paper_scissors.py
+++ 2022/day2/test_rock_paper_scissors.py
@@ -15,3 +15,17 @@ def test_part_one_input():
     result = rock_paper_scissors(file)
 
     assert result == 13675
+
+def test_part_one_input_b():
+    file = "input_b.txt"
+
+    result = rock_paper_scissors(file)
+
+    assert result == 14069
+
+def test_rock_paper_scissors_part_two():
+    file = "test_input.txt"
+
+    result = rock_paper_scissors(file, default_calc=False)
+
+    assert result == 12
