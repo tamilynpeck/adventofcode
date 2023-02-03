77c422372850c0075f267a261eab0657ef67597e
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2023-02-03 14:41:58 -0700

    test fixes

--- src/test/aoc2022/test_day1_calorie_count.py
+++ src/test/aoc2022/test_day1_calorie_count.py
@@ -1,8 +1,36 @@
 from aoc2022.day1_calorie_counting import calorie_counting
+from common.FileReader import InMemoryFileReader
+
+file_name = "year2022_day1_test.txt"
+file_data = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
 
 
 def test_elf_with_most_calories():
-    file = "year2022_day1_test.txt"
+    file_reader = InMemoryFileReader()
+    file_reader.setup(file_name, file_data)
 
-    result = calorie_counting(file)
+    result = calorie_counting(file_name, file_reader=file_reader)
+
     assert result == 24000
+
+
+def test_elf_with_most_calories_part_two():
+    file_reader = InMemoryFileReader()
+    file_reader.setup(file_name, file_data)
+
+    result = calorie_counting(file_name, file_reader=file_reader, top=3)
+
+    assert result == 45000
--- src/test/aoc2022/test_day2_rock_paper_scissors.py
+++ src/test/aoc2022/test_day2_rock_paper_scissors.py
@@ -1,37 +1,28 @@
 from aoc2022.day2_rock_paper_scissors import rock_paper_scissors
 from common.FileReader import InMemoryFileReader
 
-TEST_INPUT_FILE = "year2022_day2_test.txt"
+file_name = "year2022_day2_test.txt"
 
 test_data = """A Y
 B X
 C Z"""
 
 
-def test_memory_reader():
-    file_reader = InMemoryFileReader()
-    file_reader.setup(TEST_INPUT_FILE, test_data)
-
-    result = file_reader.read_txt(TEST_INPUT_FILE)
-
-    assert result == ["A Y", "B X", "C Z"]
-
-
 def test_rock_paper_scissors_part_one():
     file_reader = InMemoryFileReader()
-    file_reader.setup(TEST_INPUT_FILE, test_data)
+    file_reader.setup(file_name, test_data)
 
-    result = rock_paper_scissors(TEST_INPUT_FILE, file_reader=file_reader)
+    result = rock_paper_scissors(file_name, file_reader=file_reader)
 
     assert result == 15
 
 
 def test_rock_paper_scissors_part_two():
     file_reader = InMemoryFileReader()
-    file_reader.setup(TEST_INPUT_FILE, test_data)
+    file_reader.setup(file_name, test_data)
 
     result = rock_paper_scissors(
-        TEST_INPUT_FILE, guide_type="results", file_reader=file_reader
+        file_name, guide_type="results", file_reader=file_reader
     )
 
     assert result == 12
