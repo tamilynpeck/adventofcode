88a8de32fca9b50af2254d2f5a986bc7828974c0
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2023-12-04 11:08:21 -0700

    bash script fix

--- test.sh
+++ test.sh
@@ -1,25 +1,30 @@
-year=0
-all=0
+year=23
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --last) year=22 ;;
-        -a|--all) all=1 ;;;
+        -22|--22) year=22 ;;
+        -23|--23) year=23 ;;
+        -a|--all) year=0 ;;
         year) command="$1"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
+
 docker build --rm -t testing:local -f src/test/Dockerfile .
 
-if [ $year=0 ]; then
+if [ $year == 0 ]; then
+  echo "No year specified, running all tests"
   docker run testing:local
-elif [ $year=0 ]; then
+elif [ $year == 23 ]; then
+  echo "Running tests for year 2023"
   docker run --entrypoint pytest testing:local  --cov-config test/.coveragerc --cov -p no:cacheprovider test/aoc2023
-elif [ year=22 ]; then
+elif [ $year == 22 ]; then
+  echo "Running tests for year 2022"
   docker run --entrypoint pytest testing:local  --cov-config test/.coveragerc --cov -p no:cacheprovider test/aoc2022
 else
+  echo "Running default"
   docker run testing:local
 fi
 
