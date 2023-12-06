09ecad0eef645a4f0deb559552fc892c3b049afc
Tamilyn <tamilyn.peck2@extendhealth.com>
2023-12-05 22:32:14 -0700

    day6

--- src/2023/6/day6.py
+++ src/2023/6/day6.py
@@ -1,9 +1,44 @@
+from functools import reduce
+
+
 class Day6:
     def __init__(self, data):
-        self.data = data
+        self.time = data[0].split(":")[1].strip()
+        self.record_distance = data[1].split(":")[1].strip()
 
     def solve_part_one(self):
-        pass
+        time = (
+            self.time.replace("   ", " ")
+            .replace("  ", " ")
+            .replace("  ", " ")
+            .split(" ")
+        )
+        record_distance = (
+            self.record_distance.replace("   ", " ").replace("  ", " ").split(" ")
+        )
+
+        return self.calculate_options(time, record_distance)
 
     def solve_part_two(self):
-        pass
+        time = self.time.replace(" ", "").split(" ")
+        record_distance = self.record_distance.replace(" ", "").split(" ")
+        return self.calculate_options(time, record_distance)
+
+    def calculate_options(self, time, record_distance):
+        count_options = []
+        for i in range(len(time)):
+            options = Day6.button_math(int(time[i]), int(record_distance[i]))
+            count_options.append(options)
+
+        return reduce(lambda x, y: x * y, count_options)
+
+    def button_math(time, record_distance):
+        options = []
+        for i in range(time):
+            hold_time = i + 1
+            time_remaining = time - hold_time
+            distance = hold_time * time_remaining
+            if distance > record_distance:
+                options.append(distance)
+
+        return len(options)
--- src/2023/6/test_day6.py
+++ src/2023/6/test_day6.py
@@ -2,7 +2,9 @@ import pytest
 from utils import read_txt
 from day6 import Day6
 
-test_data = ""
+test_data = """Time:      7  15   30
+Distance:  9  40  200"""
+
 
 def test_day6():
     data = read_txt(test_data)
@@ -10,18 +12,23 @@ def test_day6():
 
     result = program.solve_part_one()
 
-    assert result == 0
+    assert result == 288
 
-@pytest.mark.parametrize(
-    "line,expected",
-    [
-        ("line", "expected"),
-    ],
-)
-def test_program_function(line, expected):
-    data = read_txt(line)
+
+def test_day6():
+    data = read_txt(test_data)
     program = Day6(data)
 
-    result = program.test()
+    result = program.solve_part_two()
+
+    assert result == 71503
+
+
+@pytest.mark.parametrize(
+    "time, record_distance ,expected",
+    [(7, 9, 4), (15, 40, 8), (30, 200, 9)],
+)
+def test_program_function(time, record_distance, expected):
+    result = Day6.button_math(time, record_distance)
 
     assert result == expected
