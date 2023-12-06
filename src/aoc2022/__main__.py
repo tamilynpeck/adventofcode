877dc445fd5b7f958eeea568c1f32376e1754568
Tamilyn <tamilyn.peck2@extendhealth.com>
2023-12-05 22:06:19 -0700

    oops

--- src/aoc2022/__main__.py
+++
@@ -1,9 +0,0 @@
-from aoc2022.day1_calorie_counting import calorie_counting
-
-
-PUZZLE_INPUT_FILE = "202201.txt"
-result = calorie_counting(PUZZLE_INPUT_FILE)
-print("calorie_counting part 1", result)
-
-result = calorie_counting(PUZZLE_INPUT_FILE, top=3)
-print("calorie_counting part 2", result)
