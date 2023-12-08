75e977f74936dc5c38a1b221076442c11e710b00
Tamilyn <tamilyn.peck2@extendhealth.com>
2023-12-07 23:54:27 -0700

    7 refactor, p2 wip

--- src/2023/7/day7.py
+++ src/2023/7/day7.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from string import ascii_lowercase
 
 strength = {
     "A": 12,
@@ -28,75 +27,18 @@ class Day7:
         self.wild = wild
 
     def solve_part_one(self):
-        high_cards = [hand for hand in self.hands if hand.type == 0]
-        one_pairs = [hand for hand in self.hands if hand.type == 1]
-        two_pairs = [hand for hand in self.hands if hand.type == 2]
-        three_of_a_kind = [hand for hand in self.hands if hand.type == 3]
-        full_house = [hand for hand in self.hands if hand.type == 4]
-        four_of_a_kind = [hand for hand in self.hands if hand.type == 5]
-        five_of_a_kind = [hand for hand in self.hands if hand.type == 6]
+        ranked_hands = sorted(self.hands)
+        # print([(hand.hand, hand.type_strength) for hand in ranked_hands])
 
-        high_cards = sorted(high_cards)
-        one_pairs = sorted(one_pairs)
-        two_pairs = sorted(two_pairs)
-        three_of_a_kind = sorted(three_of_a_kind)
-        full_house = sorted(full_house)
-        four_of_a_kind = sorted(four_of_a_kind)
-        five_of_a_kind = sorted(five_of_a_kind)
-
-        # ranked_hands = sorted(self.hands)
-        # rank = 1
-        # for hand in ranked_hands:
-        #     hand.rank = rank
-        #     ranked_hands.append(hand)
-        #     rank += 1
-
-        ranked_hands = []
         rank = 1
-        for hand in high_cards:
+        for hand in ranked_hands:
             hand.rank = rank
-            ranked_hands.append(hand)
+            print(hand.hand, hand.type_strength, hand.rank)
             rank += 1
-
-        for hand in one_pairs:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        for hand in two_pairs:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        for hand in three_of_a_kind:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        for hand in full_house:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        for hand in four_of_a_kind:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        for hand in five_of_a_kind:
-            hand.rank = rank
-            ranked_hands.append(hand)
-            rank += 1
-
-        # yikes = [hand for hand in ranked_hands if hand.yikes() > 1]
-        # print([(hand.hand, hand.rank) for hand in yikes])
-
-        # print([(hand.hand, hand.rank, hand.compare) for hand in ranked_hands])
 
         return sum([hand.bid * hand.rank for hand in ranked_hands])
 
     def solve_part_two(self):
-        # self.wild = True
         return self.solve_part_one()
 
 
@@ -108,81 +50,21 @@ class Hand:
         self.bid = int(bid)
         self.rank = 0
         self.wild = wild
-        self.type = None
-        if self.five_of_a_kind():
-            self.type = 6
-        if self.four_of_a_kind():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 5)
-            self.type = 5
-        elif self.full_house():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 4)
-            self.type = 4
-        elif self.three_of_a_kind():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 3)
-            self.type = 3
-        if self.two_pair():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 2)
-            self.type = 2
-        if self.one_pair():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 1)
-            self.type = 1
-        if self.high_card():
-            if self.type:
-                raise Exception("Two types found for", self.hand, self.type, 0)
-            self.type = 0
-        if self.type == None:
-            raise Exception("No type found for", self.hand)
+        self.type_strength = self.get_strength()
 
-    def five_of_a_kind(self):
-        if self.wild and "J" in self.count.keys():
-            print(len(self.count))
-            return len(self.count) <= 2
-        return 5 in self.count.values()
+    def get_strength(self):
+        if self.wild and "J" in self.count and self.count["J"] == 5:
+            return 4
+        if self.wild and "J" in self.count:
+            counts = self.count.copy()
+            j = counts.pop("J")
 
-    def four_of_a_kind(self):
-        if self.wild and "J" in self.count.keys():
-            j = self.count["J"]
-            values = [value for key, value in self.count.items() if key != "J"]
-            print(values)
-            return any([value + j == 4 for value in values])
-        return 4 in self.count.values()
-
-    def full_house(self):
-        if self.wild and "J" in self.count.keys():
-            counts = [value for key, value in self.count.items() if key != "J"]
-            return len(counts) == 2
-
-        return 3 in self.count.values() and 2 in self.count.values()
-
-    def three_of_a_kind(self):
-        if self.wild and "J" in self.count.keys():
-            j = self.count["J"]
-            return any([value + j == 3 for value in self.count.values()])
-        return 3 in self.count.values() and 2 not in self.count.values()
-
-    def two_pair(self):
-        if self.wild and "J" in self.count.keys():
-            return False
-        return 2 in self.count.values() and len(self.count) == 3
-
-    def one_pair(self):
-        if self.wild and "J" in self.count.keys():
-            return len(self.count) == 5
-        return 2 in self.count.values() and len(self.count) == 4
-
-    def high_card(self):
-        if self.wild and "J" in self.count.keys():
-            return False
-        return len(self.count) == 5
+            return max(counts.values()) + j - (len(set(self.hand)) - 1)
+        return max(self.count.values()) - len(set(self.hand))
 
     def __gt__(self, other):
-        # if self.type != other.type:
-        #     return self.type > other.type
+        if self.type_strength != other.type_strength:
+            return self.type_strength > other.type_strength
 
         if self.compare[0] != other.compare[0]:
             return self.compare[0] > other.compare[0]
--- src/2023/7/test_day7.py
+++ src/2023/7/test_day7.py
@@ -30,146 +30,122 @@ def test_day7_part_two():
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("33333", False, True),
-        ("J3333", True, True),
-        ("J3332", True, False),
-        ("J3332", False, False),
-        ("J2332", True, False),
-        ("J2332", False, False),
-        ("JJJJJ", True, True),
-        ("JJJJJ", False, True),
+        ("33333", False, 4),
+        ("J3333", True, 4),
+        # ("J3332", True, False),
+        # ("J3332", False, False),
+        # ("J2332", True, False),
+        # ("J2332", False, False),
+        ("JJJJJ", True, 4),
+        ("JJJJJ", False, 4),
     ],
 )
 def test_five_of_a_kind(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 6
-    result = hand.five_of_a_kind()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("33332", False, True),
-        ("33333", False, False),
-        ("32233", False, False),
-        ("J3332", False, False),
-        ("3KTJJ", True, False),
-        ("J3332", True, True),
-        ("332JJ", True, True),
+        ("33332", False, 2),
+        # ("33333", False, False),
+        # ("32233", False, False),
+        # ("J3332", False, False),
+        # ("3KTJJ", True, False),
+        ("J3332", True, 2),
+        ("332JJ", True, 2),
     ],
 )
 def test_four_of_a_kind(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 5
-    result = hand.four_of_a_kind()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("23332", False, True),
-        ("32T3K", False, False),
-        ("22222", False, False),
-        ("J2222", True, False),
-        ("2J332", True, True),
-        ("J3322", True, True),
-        ("32T3J", True, False),
+        ("23332", False, 1),
+        # ("32T3K", False, False),
+        # ("22222", False, False),
+        # ("J2222", True, False),
+        ("2J332", True, 1),
+        ("J3322", True, 1),
+        # ("32T3J", True, False),
     ],
 )
 def test_full_house(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 4
-    result = hand.full_house()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("QQQJA", False, True),
-        ("32T3K", False, False),
-        ("23332", False, False),
-        ("32T3J", True, True),
-        ("33T4J", True, True),
+        ("QQQJA", False, 0),
+        # ("32T3K", False, False),
+        # ("23332", False, False),
+        ("32T3J", True, 0),
+        ("33T4J", True, 0),
     ],
 )
 def test_three_of_a_kind(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 3
-    result = hand.three_of_a_kind()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("23432", False, True),
-        ("32T3K", False, False),
-        ("23332", False, False),
-        ("3KT4J", True, False),
-        ("33T4J", True, False),
-        ("3KTJJ", True, False),
+        ("23432", False, -1),
+        # ("32T3K", False, False),
+        # ("23332", False, False),
+        # ("3KT4J", True, False),
+        # ("33T4J", True, False),
+        # ("3KTJJ", True, False),
     ],
 )
 def test_two_pair(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 2
-    result = hand.two_pair()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("2K432", False, True),
-        ("32T9K", False, False),
-        ("23332", False, False),
-        ("23432", False, False),
-        ("3KT4J", True, True),
-        ("3KT99", True, True),
-        ("J37QA", True, True),
-        ("J37QA", False, False),
+        ("2K432", False, -2),
+        # ("32T9K", False, False),
+        # ("23332", False, False),
+        # ("23432", False, False),
+        ("3KT4J", True, -2),
+        ("3KT99", True, -2),
+        ("J37QA", True, -2),
+        # ("J37QA", False, False),
     ],
 )
 def test_one_pair(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 1
-    result = hand.one_pair()
 
-    assert result == expected
+    assert hand.type_strength == expected
 
 
 @pytest.mark.parametrize(
     "hand,wild,expected",
     [
-        ("2K439", False, True),
-        ("32T9K", False, True),
-        ("3KT4J", False, True),
-        ("2K439", True, True),
-        ("32T9K", True, True),
-        ("23332", False, False),
-        ("23432", False, False),
-        ("3KT9J", True, False),
-        ("J37QA", False, True),
+        ("2K439", False, -4),
+        ("32T9K", False, -4),
+        ("3KT4J", False, -4),
+        ("2K439", True, -4),
+        ("32T9K", True, -4),
+        ("J37QA", False, -4),
     ],
 )
 def test_high_card(hand, wild, expected):
     hand = Hand(hand, 0, wild=wild)
-    if expected:
-        assert hand.type == 0
-    result = hand.high_card()
 
-    assert result == expected
+    assert hand.type_strength == expected
