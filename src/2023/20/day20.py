92fecf79cf8f5ead3f6ed168824d5b36c12d3b70
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2023-12-20 18:17:53 -0700

    day20 setup

---
+++ src/2023/20/day20.py
@@ -0,0 +1,10 @@
+class Day20:
+    def __init__(self, data):
+        self.data = data
+        self.broadcaster = [l for l in data if "broadcaster" in l][0]
+
+    def solve_part_one(self):
+        pass
+
+    def solve_part_two(self):
+        pass
