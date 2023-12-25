840bad36f648269860b5f7b8a43946f082bd35ac
Tamilyn <tamilyn.peck2@extendhealth.com>
2023-12-25 02:36:40 -0700

    day25 part1

---
+++ src/2023/25/day25.py
@@ -0,0 +1,36 @@
+import networkx as nx
+
+
+class Day25:
+    def __init__(self, data):
+        self.data = data
+        self.components = {
+            component.strip(): others.strip().split(" ")
+            for component, others in [line.split(":") for line in self.data]
+        }
+
+    def solve_part_one(self):
+        graph = nx.Graph()
+        for component, others in self.components.items():
+            for other in others:
+                graph.add_edge(component, other, capacity=1)
+
+        print(graph.nodes)
+        print(graph.edges)
+        partition_counts = []
+        for edge in graph.edges:
+            # print(edge)
+            cut_value, partition = nx.minimum_cut(graph, edge[0], edge[1])
+            if cut_value == 3 and len(partition) == 2:
+                print(cut_value)
+                print(partition)
+                counts = [len(x) for x in partition]
+                partition_counts = counts
+                print(counts)
+                break
+
+        print(graph)
+        return partition_counts[0] * partition_counts[1]
+
+    def solve_part_two(self):
+        pass
---
+++ src/2023/25/test_day25.py
@@ -0,0 +1,41 @@
+import pytest
+from utils import read_txt
+from day25 import Day25
+
+test_data = """jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr"""
+
+
+def test_day25():
+    data = read_txt(test_data)
+    program = Day25(data)
+
+    result = program.solve_part_one()
+
+    assert result == 54
+
+
+# @pytest.mark.parametrize(
+#     "line,expected",
+#     [
+#         ("line", "expected"),
+#     ],
+# )
+# def test_program_function(line, expected):
+#     data = read_txt(line)
+#     program = Day25(data)
+
+#     result = program.test(line)
+
+#     assert result == expected
