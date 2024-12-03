c5516d52b722bef1665a22dcfc7f567fcaf1c2d0
Tamilyn Norton <65209760+tamilynpeck@users.noreply.github.com>
2024-12-03 09:30:57 -0700

    day 3 2024

--- src/2024/3/day3.py
+++ src/2024/3/day3.py
@@ -1,9 +1,28 @@
+import re
+
+
 class Day3:
     def __init__(self, data):
-        self.data = data
+        self.data = "".join(data)
 
     def solve_part_one(self):
-        pass
+        regex = r"mul\((\d+),(\d+)\)"
+        matches = re.findall(regex, self.data)
+        result = [int(match[0]) * int(match[1]) for match in matches]
+        return sum(result)
 
     def solve_part_two(self):
-        pass
+        regex = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+        matches = re.findall(regex, self.data)
+        result = 0
+        enabled = True
+        for match in matches:
+            if match == "do()" :
+                enabled = True
+            elif match == "don't()":
+                enabled = False
+            elif enabled:
+                match = re.findall(r"\d+", match)
+                result += int(match[0]) * int(match[1])
+
+        return result
--- src/2024/3/test_day3.py
+++ src/2024/3/test_day3.py
@@ -2,26 +2,21 @@ import pytest
 from utils import read_txt
 from day3 import Day3
 
-test_data = ""
+test_data = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
 
-def test_day3():
+def test_part_one():
     data = read_txt(test_data)
     program = Day3(data)
 
     result = program.solve_part_one()
 
-    assert result == 0
+    assert result == 161
 
-@pytest.mark.parametrize(
-    "line,expected",
-    [
-        ("line", "expected"),
-    ],
-)
-def test_program_function(line, expected):
-    data = read_txt(line)
+
+def test_part_two():
+    data = read_txt(test_data)
     program = Day3(data)
 
-    result = program.test(line)
+    result = program.solve_part_two()
 
-    assert result == expected
+    assert result == 48
