44149ed119b24b1e37313ea902d37d711a75286b
Tamilyn <tl.tamilyn@gmail.com>
2025-12-02 00:49:39 -0700

    day 2 part 2

--- src/2025/2/day2.py
+++ src/2025/2/day2.py
@@ -8,15 +8,23 @@ class Day2:
         invalid_values = []
 
         for id_range in id_ranges:
-            invalid_ids = self.valid_id(id_range)
+            invalid_ids = self.invalid_id(id_range)
             invalid_values.extend(invalid_ids)
 
         return sum(invalid_values)
 
     def solve_part_two(self):
-        pass
+        id_ranges = self.data[0].split(",")
 
-    def valid_id(self, line):
+        invalid_values = []
+
+        for id_range in id_ranges:
+            invalid_ids = self.invalid_id_part_two(id_range)
+            invalid_values.extend(invalid_ids)
+
+        return sum(invalid_values)
+
+    def invalid_id(self, line):
         starting, ending = map(int, line.split("-"))
         invalid_ids = []
 
@@ -26,5 +34,29 @@ class Day2:
             half = len(num_str) // 2
             if num_str[:half] == num_str[half:]:
                 invalid_ids.append(num)
+
+        return invalid_ids
+
+    def invalid_id_part_two(self, line):
+        starting, ending = map(int, line.split("-"))
+        invalid_ids = []
+
+        for num in range(starting, ending + 1):
+            num_str = str(num)
+            len_num = len(num_str)
+            # Try different numbers of parts (divisors of length)
+            for num_parts in range(2, len_num + 1):
+                if len_num % num_parts == 0:
+                    part_size = len_num // num_parts
+                    parts = [
+                        num_str[j * part_size : (j + 1) * part_size]
+                        for j in range(num_parts)
+                    ]
+                else:
+                    continue
+
+                if len(set(parts)) == 1:
+                    invalid_ids.append(num)
+                    break
 
         return invalid_ids
--- src/2025/2/test_day2.py
+++ src/2025/2/test_day2.py
@@ -15,13 +15,13 @@ def test_part_one():
     assert result == 1227775554
 
 
-# def test_part_two():
-#     data = read_txt(test_data)
-#     program = Day2(data)
+def test_part_two():
+    data = read_txt(test_data)
+    program = Day2(data)
 
-#     result = program.solve_part_two()
+    result = program.solve_part_two()
 
-#     assert result == 0
+    assert result == 4174379265
 
 
 @pytest.mark.parametrize(
@@ -43,6 +43,30 @@ def test_part_one():
 def test_program_function(id_range, expected):
     program = Day2(test_data)
 
-    result = program.valid_id(id_range)
+    result = program.invalid_id(id_range)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "id_range,expected",
+    [
+        ("11-22", [11, 22]),
+        ("95-115", [99, 111]),
+        ("998-1012", [999, 1010]),
+        ("1188511880-1188511890", [1188511885]),
+        ("222220-222224", [222222]),
+        ("1698522-1698528", []),
+        ("446443-446449", [446446]),
+        ("38593856-38593862", [38593859]),
+        ("565653-565659", [565656]),
+        ("824824821-824824827", [824824824]),
+        ("2121212118-2121212124", [2121212121]),
+    ],
+)
+def test_program_function_part_two(id_range, expected):
+    program = Day2(test_data)
+
+    result = program.invalid_id_part_two(id_range)
 
     assert result == expected
